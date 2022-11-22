f7b56817f2e15d2fa8b43fbce85855ffca320e07
EliusCaleb <eliuswanyoro@gmail.com>
2022-11-22 09:08:27 +0300

    Learning

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,5 +1,7 @@
 class PlantsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
+  
   # GET /plants
   def index
     plants = Plant.all
@@ -8,7 +10,7 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
+    plant = find_plant
     render json: plant
   end
 
@@ -20,29 +22,27 @@ class PlantsController < ApplicationController
 
   # PATCH /plants/:id
 def update
-  plant = Plant.find_by(id: params[:id])
-  if plant
-    plant.update(plant_params)
-    render json: plant
-  else
-    render json: { error: "Plant not found" }, status: :not_found
-  end
+  plant = find_plant
+  plant.update(plant_params)
+  render json: plant
 end
 # DELETE /plants/:id
 def destroy
-  plant = Plant.find_by(id: params[:id])
-  if plant
-    plant.destroy
-    head :no_content
-  else
-    render json: { error: "Plant not found" }, status: :not_found
-  end
+  plant = find_plant
+  plant.destroy
+  head :no_content
 end
 
 
   private
+  def find_plant
+    Plant.find(params[:id])
+  end
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
+  end
+  def render_not_found_response
+    render json: { error: "Plant not found" }, status: :not_found
   end
 end
